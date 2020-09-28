b534740c0deebeb3e0e6ec8cf8bc3c27e6b620e3
youn yeo won <54175366+dudnjsckrgo@users.noreply.github.com>
2020-09-28 13:04:34 +0900

    15:15

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './components/Counter'
+const App = () => {
+  return <>
+    <h1>TODO-APP</h1>
+    <div>
+      <Counter/>
+    </div>
+  </>
+}
+
+export default App;
